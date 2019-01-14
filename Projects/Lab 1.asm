60c085696c5bb181a2549d3043119c9f85ad161d
Johnathan Machler <32213433+esch3r@users.noreply.github.com>
2019-01-14 03:16:38 -0600

    Rename Lab 2.asm to Lab 1.asm

---
+++ Projects/Lab 1.asm
@@ -0,0 +1,41 @@
+Begin:  ldi r16,High(RAMEND)
+        out SPH,r16
+        ldi r16,Low(RAMEND)
+        out SPL,r16 
+        ldi r16,0b11111111
+        out DDRA,r16
+        out DDRC,r16
+        out DDRD,r16
+        rcall LCDini
+        rcall LCDini
+        rcall LCDstr
+        .db "EE2325:Welcome!",$80,0
+        
+Back:
+
+      ldi r16,0b000010000
+      out PORTA,r16
+      ldi r16,0b01011110
+      out PORTC,r16
+      
+Delay1:
+       dec r17 
+       brne Delay1
+       ldi r16,0b000000100
+       out PORTA,r16
+       ldi r16,0b11010000 
+       out PORTC,r16 
+       
+Delay2: dec r17 
+        brne Delay2 
+        ldi r16,0b00000010
+        out PORTA,r16 
+        ldi r16,0b10111001
+        out PORTC,r16
+
+Delay3:  
+       dec r17
+       brne Delay3
+       rjmp Back
+       
+       .include "C:\Users\student\documents\LCDstuff.asm"
